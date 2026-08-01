9325d8a6d99d7e6874a5cbbda978c415e9fbafd2
XenaScripts <indraxslzr@gmail.com>
2026-08-01 21:42:03 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,8 +1,8 @@
 local function s1() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/OXMz88oz35/0", true))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/BtBhUZHTY1/0", true))()
 end 
 local function s2() 
-    loadstring(game:HttpGet("https://pastefy.app/MrvANzqw/raw"))()
+    loadstring(game:HttpGet("https://pastefy.app/MrvANzqw/raw", true))()
 end
 
 task.spawn(s1)
