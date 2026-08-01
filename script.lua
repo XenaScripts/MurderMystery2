4bab5a75a27293800b37911db34c717e19bf3345
XenaScripts <indraxslzr@gmail.com>
2026-08-01 20:59:33 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local function s1()
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/OXMz88oz35/0", true))()
 end 
 local function s2() 
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/XenaScripts/murdermyster2.xenav2/refs/heads/main/ScriptV2_obfuscated.lua'))()
+    loadstring(game:HttpGet("https://pastefy.app/ODPQb596/raw"))()
 end
 
 task.spawn(s1)
