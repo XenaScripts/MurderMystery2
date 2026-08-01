4d1b6ffa105e89c70925e068cfbb36f4a49072c4
XenaScripts <indraxslzr@gmail.com>
2026-08-01 21:58:58 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,8 +1,8 @@
 local function s1() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/BtBhUZHTY1/0", true))()
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImU3NTA2NmI2LWUyNDctNDZhZS05NjBhLWIyZjVhMjY0MDhjZCIsImtpbmQiOiJsb2FkZXIifQ"))()
 end 
 local function s2() 
-    loadstring(game:HttpGet("https://pastefy.app/MrvANzqw/raw", true))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/XenaScripts/murdermyster2.xenav2/refs/heads/main/ScriptV2_obfuscated.lua'))()
 end
 
 task.spawn(s1)
