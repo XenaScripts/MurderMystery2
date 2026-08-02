27fff73b619752dfa96f01eb4e681ffa775b19c1
XenaScripts <indraxslzr@gmail.com>
2026-08-02 22:44:24 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local function s1() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/BtBhUZHTY1/0", true))()
+    loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImU3NTA2NmI2LWUyNDctNDZhZS05NjBhLWIyZjVhMjY0MDhjZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20ifX0"))()
 end 
 local function s2() 
     loadstring(game:HttpGet('https://pastefy.app/WJALYUg2/raw'))()
