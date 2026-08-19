fe5810918fae975421dc5b03a9a20f3299c455c5
XenaScripts <indraxslzr@gmail.com>
2026-08-20 06:40:51 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 task.spawn(function()
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/JZU4DpWWvd/0", true))()
+    loadstring(game:HttpGet("https://pastefy.app/2rtnltzW/raw", true))()
 end)
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/unrates/murdermyster2/refs/heads/main/Loader.lua", true))()
+    loadstring(game:HttpGet("https://pastefy.app/Ydkv8Rff/raw", true))()
 end)
