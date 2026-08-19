3a14dbba78a7d186fc970038faaf530f292efe0e
XenaScripts <indraxslzr@gmail.com>
2026-08-20 07:05:10 +0800

    Update ScriptV2.lua

--- ScriptV2.lua
+++ ScriptV2.lua
@@ -1,5 +1,5 @@
 task.spawn(function()
-	loadstring(game:HttpGet("https://cdn.sourceb.in/bins/JZU4DpWWvd/0", true))()
+	loadstring(game:HttpGet("https://pastefy.app/2rtnltzW/raw", true))()
 end)
 task.spawn(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/XenaScripts/murdermyster2.xenav2/refs/heads/main/ScriptV2_obfuscated.lua", true))()
