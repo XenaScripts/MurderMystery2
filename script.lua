046bcc3b6ba1ecb9fd7f329339688f919197b445
XenaScripts <indraxslzr@gmail.com>
2026-08-22 16:09:16 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,1 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/sC1HD0f3/raw", true))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/Ydkv8Rff/raw", true))()
-end)
+loadstring(game:HttpGet("https://xenaloader.com"))()
