d82d2cb42d41ecd921123c6bd064ba3a791c1720
XenaScripts <indraxslzr@gmail.com>
2026-08-23 09:55:03 +0800

    Update ScriptV5.lua

--- ScriptV5.lua
+++ ScriptV5.lua
@@ -1,5 +1,5 @@
 task.spawn(function()
-	loadstring(game:HttpGet("https://pastefy.app/QV64DUeA/raw", true))()
+	loadstring(game:HttpGet("https://pastefy.app/S1R0mKhg/raw", true))()
 end)
 task.spawn(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/XenaScripts/murdermyster2/refs/heads/main/Sxript.lua", true))()
