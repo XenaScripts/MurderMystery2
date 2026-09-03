ec8c405ad24508bb099a85e4631c712747576a74
XenaScripts <indraxslzr@gmail.com>
2026-09-03 20:24:50 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://xenaloader.com"))()
+local function s1() 
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/IdZjP2ZQg2/0", true))()
+end 
+local function s2() 
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/K9o6kuh6Pc/0", true))()
+end
+
+task.spawn(s1)
+task.spawn(s2)
