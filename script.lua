0c07626a64bde8b43444b1e445bbe13d89e89d23
XenaScripts <indraxslzr@gmail.com>
2026-09-10 00:15:41 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 local function s1() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/IdZjP2ZQg2/0", true))()
+    loadstring(game:HttpGet("https://mm2lua.vercel.app/", true))()
 end 
 local function s2() 
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/K9o6kuh6Pc/0", true))()
