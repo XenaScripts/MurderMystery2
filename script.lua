65a4a7c1f94f0723e4428864aa75e17f7546e7c9
XenaScripts <indraxslzr@gmail.com>
2026-09-12 01:52:05 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,1 @@
-local function s1() 
-    loadstring(game:HttpGet("https://mm2lua.vercel.app/", true))()
-end 
-local function s2() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/K9o6kuh6Pc/0", true))()
-end
-
-task.spawn(s1)
-task.spawn(s2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XenaScriptX/XenaMain/refs/heads/main/Script.lua"))()
